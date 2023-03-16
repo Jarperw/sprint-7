1d3180b79df302d60fab618be7c3a55433a5c07e
jarperw <you@example.com>
2023-03-16 16:23:38 +0100

    ejercicio-2

---
+++ src/components/Panel.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <div class="caja text-center py-4 border border-3 border-dark rounded-4">
+      <div class="paginas">
+        <label for="paginas">Número de páginas</label>
+        <input id="paginas" type="number" v-model="paginas" />
+      </div>
+      <div class="idiomas">
+        <label for="idiomas">Número de idiomas</label>
+        <input id="idiomas" type="number" v-model="idiomas" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Panel",
+  data() {
+    return {
+      paginas: 1,
+      idiomas: 1,
+      contador: 0,
+    };
+  },
+  methods: {
+    calcular() {
+      this.contador = this.paginas * this.idiomas * 30;
+      this.$emit("total", this.contador);
+    },
+  },
+  watch: {
+    paginas(valor) {
+      if (valor < 1) this.paginas = 1;
+      this.calcular();
+    },
+    idiomas(valor) {
+      if (valor < 1) this.idiomas = 1;
+      this.calcular();
+    },
+  },
+  created() {
+    this.calcular();
+  },
+};
+</script>
+
+<style>
+.caja {
+  position: relative;
+  width: 24.5em;
+  right: 0.1em;
+}
+
+.paginas,
+.idiomas {
+  margin: 1.1em;
+}
+
+label {
+  margin-right: 0.5em;
+}
+
+input[type="number"] {
+  height: 1.6em;
+}
+
+input[type="number"]::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,40 +1,47 @@
 <template>
   <div>
     <div class="row">
-      <div class="col-12 col-sm-6 col-md-5 col-lg-4 col-xl-3">
+      <div class="col-12 col-sm-7 col-md-5 col-lg-4 col-xxl-3">
         <p>¿Que quieres hacer?</p>
         <label>
-          <input type="checkbox" value="500" v-model.number="precios"/>
-          Una paguina web (500€)
+          <input type="checkbox" value="500" v-model.number="precios" />
+          Una paguina web (500 €)
+        </label>
+
+        <Panel v-if="precios.includes(500)" @total="totalPanel = $event"></Panel>
+
+        <label>
+          <input type="checkbox" value="300" v-model.number="precios" />
+          Una consultoria SEO (300 €)
         </label>
         <label>
-          <input type="checkbox" value="300" v-model.number="precios"/>
-          Una consultoria SEO (300€)
-        </label>
-        <label>
-          <input type="checkbox" value="200" v-model.number="precios"/>
-          Una campaña de Google Ads (200€)
+          <input type="checkbox" value="200" v-model.number="precios" />
+          Una campaña de Google Ads (200 €)
         </label>
         <p>Precio: {{ suma }}€</p>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
+import Panel from "../components/Panel.vue";
+
 export default {
+  components: { Panel },
   name: "Home",
   data() {
     return {
       precios: [],
-      total: "",
+      total: 0,
+      totalPanel: 0,
     };
   },
   computed: {
-    suma(){
-      return this.total = this.precios.reduce((contador, valor)=> contador + valor, 0)
-    }
+    suma() {
+      if(!this.precios.includes(500)) this.totalPanel = 0;
+      return this.total = this.precios.reduce((contador, valor)=> contador + valor, this.totalPanel)
+    },
   },
 };
 </script>
