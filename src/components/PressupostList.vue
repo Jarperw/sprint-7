6d944aac579e997c1c5e5b9588c4ff1e6af91ab7
jarperw <you@example.com>
2023-03-17 12:33:26 +0100

    ejercicio-6

---
+++ src/components/PressupostList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="mt-5 mt-md-0">
+    <b-alert
+      show
+      v-for="(item, index) in presupuestos"
+      :key="index"
+      variant="secondary"
+    >
+      <div class="mx-5 d-flex justify-content-around">
+        <span><b>Presupuesto: </b>{{ item.presupuesto | mayuscula }}</span>
+        <span><b>Cliente: </b>{{ item.cliente | mayuscula}}</span>
+        <span><b>Precio: </b>{{ item.total }} €</span>
+      </div>
+    </b-alert>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PressupostList",
+  props: ["presupuestos"],
+  filters:{
+    mayuscula(valor){
+        return valor.charAt(0).toUpperCase() + valor.slice(1)
+    }
+  }
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,44 @@
 <template>
   <div class="container-fluid mt-4">
     <div class="row">
-      <div class="col-12 col-sm-7 col-md-5 col-lg-4 col-xxl-3">
-        <p>¿Qué quieres hacer?</p>
-        <label>
-          <input type="checkbox" value="500" v-model.number="precios" />
-          Una página web (500 €)
-        </label>
 
-        <Panel v-if="precios.includes(500)" @total="totalPanel = $event"></Panel>
+      <div class="col-12 col-sm-7 col-md-5 col-lg-4 col-xxl-3">  
+        <form @submit.prevent="addPresupuestos()">
+          <div class="nombre d-flex mb-1">
+            <label for="nom"><b>Presupuesto:</b></label>
+            <input class="ms-2" type="text" id="nom" placeholder="Nombre Presupuesto" v-model="presupuesto" required>
+          </div>
+          <div class="cliente d-flex mb-3">
+            <label for="clie"><b>Cliente:</b></label>
+            <input class="ms-5" type="text" id="clie" placeholder="Nombre Cliente" v-model="cliente" required>
+          </div>
+          <button class="btn btn-dark mb-5">Guardar Presupuesto</button>
+        </form>
+          <div class="servicios">
+            <p>
+              <b>Servicios: </b>
+            </p>
+            <p>¿Qué quieres hacer?</p>
+            <label>
+              <input type="checkbox" value="500" v-model.number="precios"/>
+              Una página web (500 €)
+            </label>
+            <Panel v-if="precios.includes(500)" @total="totalPanel = $event"></Panel>
+            <label>
+              <input type="checkbox" value="300" v-model.number="precios"/>
+              Una consultoria SEO (300 €)
+            </label>
+            <label>
+              <input type="checkbox" value="200" v-model.number="precios"/>
+              Una campaña de Google Ads (200 €)
+            </label>
+            <p>Precio: {{ suma }} €</p>
+            <small :v-show="error" class="text-danger">{{error}}</small>
+          </div>
+      </div>
 
-        <label>
-          <input type="checkbox" value="300" v-model.number="precios" />
-          Una consultoria SEO (300 €)
-        </label>
-        <label>
-          <input type="checkbox" value="200" v-model.number="precios" />
-          Una campaña de Google Ads (200 €)
-        </label>
-        <p>Precio: {{ suma }}€</p>
+      <div class="col-12 col-md">
+        <Pressupost-list :presupuestos="presupuestos"></Pressupost-list>
       </div>
     </div>
   </div>
@@ -26,31 +46,60 @@
 
 <script>
 import Panel from "../components/Panel.vue";
+import PressupostList from '../components/PressupostList.vue';
 
 export default {
-  components: { Panel },
+  components: { Panel, PressupostList },
   name: "Home",
   data() {
     return {
       precios: [],
       total: 0,
       totalPanel: 0,
+      presupuesto: '',
+      cliente: '',
+      presupuestos: [],
+      error: ''
     };
   },
   computed: {
     suma() {
       if(!this.precios.includes(500)) this.totalPanel = 0;
       return this.total = this.precios.reduce((contador, valor)=> contador + valor, this.totalPanel)
-    },
+    }
+  },
+  methods: {
+    addPresupuestos(){
+      if(this.precios.length > 0){
+        this.presupuestos.push(
+          { presupuesto: this.presupuesto, cliente: this.cliente, total: this.total}
+        );
+        this.error = '';
+        this.presupuesto = '';
+        this.cliente = '';
+        this.precios = [];
+      }else{
+        this.error = 'Elige minimo un servicio';
+      }
+    }
   },
 };
 </script>
 
-
 <style scoped>
-label {
+.servicios label {
   display: block;
   margin-top: 0.6em;
   margin-bottom: 0.6em;
+}
+
+.nombre input, 
+.cliente input, 
+select{
+  /* width: 100%; */
+  border: none;
+  border-bottom: 1px solid #aaa;
+  /* border-radius: 0.6em; */
+   outline: none;
 }
 </style>
